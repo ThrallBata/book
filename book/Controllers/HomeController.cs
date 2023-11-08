39b7c0e5cfb3dded73d3f4d27a550cc88ef73bae
Pc1 <Pc1@DESKTOP-JKF2V7C>
2023-11-08 15:31:06 +0700

    Add project files.

---
+++ book/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using book.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace book.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration Configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var adminName = Configuration.GetSection("Admin:Name");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult About()
+        {
+            int age = 228;
+            string name = "Andrey";
+            var user = new User { name = name, age = age };
+            return View(age);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
